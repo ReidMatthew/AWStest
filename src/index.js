e9c81dd8fbd25aa394abf23150442f19572e7c7a
Matt Reid <msreid92@gmail.com>
2024-04-02 14:10:39 -0400

    added collapsable menu and routes

--- src/index.js
+++ src/index.js
@@ -4,21 +4,44 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { BrowserRouter as Router, Route, Routes, Link, Switch } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sidebar>
-      <Menu>
-        <SubMenu label="Charts">
-          <MenuItem> Pie charts </MenuItem>
-          <MenuItem> Line charts </MenuItem>
-        </SubMenu>
-        <MenuItem> Documentation </MenuItem>
-        <MenuItem> Calendar </MenuItem>
-      </Menu>
-    </Sidebar>;
-    <App />
+    <Router>
+      <div className="App">
+        <Sidebar style={{ height: "100vh" }} collapsed={false}>
+          <Menu>
+            <SubMenu label="Charts">
+              <MenuItem component={<Link to="/contact" /> }> Contact </MenuItem>
+              <MenuItem> Line charts </MenuItem>
+            </SubMenu>
+            <MenuItem>
+              <Link to="/">Home</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/blogs">Blogs</Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/contact">Contact</Link>
+            </MenuItem>
+          </Menu>
+        </Sidebar>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        
+      </div>
+    </Router>
   </React.StrictMode>
 );
 
